80671830783b5f14b912ae3ad04857f788bc3922
Vitória de Oliveira <vitoria.oliveira513@gmail.com>
2026-01-22 22:39:39 -0300

    feat(payment): adicionar lógica de busca e criação de cliente na api

--- app/services/asaas_service.py
+++ app/services/asaas_service.py
@@ -1,44 +1,59 @@
-from ...database.database_manager import get_db
-import logging, os, requests
+import logging
+import os
+import requests
+import re
+from database.database_manager import get_db
 
 
 logger = logging.getLogger(__name__)
 
 
-def cria_ou_consultaCliente(dados_pessoais):
+def cria_ou_consulta_cliente(dados_pessoais):
     db = get_db()
     cursor = db.cursor()
 
     try:
         cpf_cnpj = dados_pessoais["cpfCnpj"]
 
-        cursor.execute("select id_cliente_asaas from cliente where cpf_cnpj = %s", (cpf_cnpj,))
+        cursor.execute(
+            "select id_cliente_asaas from cliente where cpf_cnpj = %s", (cpf_cnpj,)
+        )
         resultado = cursor.fetchone()
 
-        if resultado:
+        if resultado and resultado[0]:
             id_cliente_asaas = resultado[0]
             logging.info(f"ID existente: {id_cliente_asaas}")
             return id_cliente_asaas
         else:
-            url = "https://sandbox.asaas.com/api/v3/customers"
+            url = "https://api-sandbox.asaas.com/v3/customers"
             headers = {
-                "Content-Type": "application/json",
-                "access_token": os.environ.get('ACCESS_TOKEN'),
+                "content-type": "application/json",
+                "accept": "application/json",
+                "access_token": os.environ.get("ACCESS_TOKEN"),
             }
 
             response = requests.post(url, json=dados_pessoais, headers=headers)
 
             if response.status_code == 200:
                 dados_asaas = response.json()
-                novo_id_asaas = dados_asaas['id']
+                novo_id_asaas = dados_asaas["id"]
 
-                #logica para salvar aqui
+                nome = dados_asaas["name"]
+                email = dados_asaas["email"]
+                cpfCnpj = dados_asaas["cpfCnpj"]
+                cep = dados_asaas["postalCode"]
+                num_residencia = dados_asaas["addressNumber"]
+                id_cliente_asaas = dados_asaas["id"]
+
+                cursor.execute(
+                    "insert into cliente (nome, email, cpf_cnpj, cep, num_residencia, id_cliente_asaas) values (%s, %s, %s, %s, %s, %s)", (nome, email, cpfCnpj, cep, num_residencia, id_cliente_asaas)
+                )
+                db.commit()
 
                 return novo_id_asaas
             else:
                 # Trate o erro (ex: CPF inválido)
                 return None
-        
 
     except Exception as e:
         db.rollback()
@@ -46,4 +61,3 @@ def cria_ou_consultaCliente(dados_pessoais):
         raise e
     finally:
         cursor.close()
-
