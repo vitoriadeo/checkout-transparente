985a1d6a1a2aa1aa758d19fc12973ba761212912
Vitória de Oliveira <vitoria.oliveira513@gmail.com>
2026-02-09 18:15:32 -0300

    feat(remove-cc-method): remove credit card method for payments
    will be added in the future

--- app/static/js/togglePayment.js
+++ app/static/js/togglePayment.js
@@ -1,10 +1,8 @@
 
 function togglePayment(method) {
-    document.getElementById('payment-card').style.display = 'none';
     document.getElementById('payment-pix').style.display = 'none';
     document.getElementById('payment-boleto').style.display = 'none';
 
-    document.getElementById('lbl-card').classList.remove('selected');
     document.getElementById('lbl-pix').classList.remove('selected');
     document.getElementById('lbl-boleto').classList.remove('selected');
 
